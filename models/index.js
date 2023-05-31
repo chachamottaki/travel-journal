d9e86e10dd3383f05091846129b2c78ebb71516f
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-05-31 18:03:59 +0200

    relations between modes: user-journal, entry-journal

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db.js');
+
+//db object is created to store the models and associations
+const db = {};
+
+//assign imported Sequelize and sequelize instances to the db object
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.Journal = require('../models/groupModel');
+db.User = require('../models/userModel');
+db.Entries =  require('../models/courseModel');
+
+// establishing the associations between the models
+
+db.User.hasOne( db.Journal, { foreignKey: "user_id" });
+db.Entry.belongsTo( db.Journal, { foreignKey: "journal_id" });
+//db.Journal.hasMany( db.Entry, { foreignKey: "journal_id" });
+
+//theme
+//db.Theme.hasMany( db.Journal, { foreignKey: "theme_id" });
+module.exports = db
